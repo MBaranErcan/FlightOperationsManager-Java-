4143b2175416c8129c903d507596c5526b7d9a48
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2023-04-16 19:31:08 +0300

    Update Main.java

--- FlightOperationsManager/src/Main.java
+++ FlightOperationsManager/src/Main.java
@@ -1,96 +1,87 @@
-import java.lang.Exception;
-import java.util.Scanner;
+//-----------------------------------------------------
+// Title: Main class
+// Author: Mustafa Baran Ercan
+// ID: 28810555206
+// Section: 1
+// Homework: 1
+// Description: This class is the main class.
+//-----------------------------------------------------
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        
-        Graph<Integer> graph = new Graph<Integer>();    
-        
-        getInput(graph);
-    
-    }
-    
-    public static void getInput(Graph graph) {                         // Gets the input from user
-        Scanner input = new Scanner(System.in);
-        
         try {
-            String line = input.nextLine();                 // Creates an String to hold the first input line which includes N M T C values.
-            String[] firstLine = (line.split(" "));         // Splits the N M T C values.
+            Scanner scanner = new Scanner(System.in);// - Get N, M, T, C values from the user.
+  
+            int N = scanner.nextInt();  // - N represents the number of cities (vertices).
+            int M = scanner.nextInt();  // - M represents the number of connections (edges).
+            int T = scanner.nextInt();  // - T represents the time required by airports to change their states.
+            int C = scanner.nextInt();  // - C represents the time for travelling one city to another.
             
-            Integer[] nmtc = new Integer[4];                // We hold the N M T C values in an Integer array each corresponding to their values.
+            // - Instantiate the graph with N vertices.
+            Graph graph = new Graph(N);
             
-            for (int i = 0; i < 4; i++) {
-                nmtc[i] = Integer.valueOf(firstLine[i]);    // Assigns the N M T C values to variables called n m t c.
+            // - Get edges (roads) from the user.
+            for (int i = 0; i < M; i++) {
+                int v = scanner.nextInt();
+                int u = scanner.nextInt();
+                
+                checkValidVertex(graph, v, u);  // - Check if the vertex is valid.
+                graph.addEdge(v, u);    // - If they are, then add them to the graph.
             }
-            if ((nmtc[1]) > (nmtc[0])) {
-                System.out.println("There cannot be more bidirectional roads than the number of airports!"); System.exit(0); // Logical check.
+            
+            // - Get the source and destination vertices.
+            int source = scanner.nextInt();
+            int destination = scanner.nextInt();
+            
+            
+            List<Integer> path = new LinkedList<>(); // -List to store the shortest path from source to destination.
+            int dist = -1;  // - Int value to store the distance value.
+            // - Find the shortest path and distance using BFS algorithm.
+            dist = graph.BFS(source, destination, path);
+            
+            // - Calculate the required time.
+            int time = 0;
+            if(dist == 1) {
+                time = T;
+            } else {
+                // - Mathematical calculations to find the time.
+                int t_temp = T;
+                for (int i = 0; i < dist; i++) {
+                    if (t_temp <= 0){
+                        time += (T-(Math.abs(t_temp)));
+                        t_temp = T;
+                    }
+                    time += C;
+                    t_temp -= C;   
+                }
             }
+            // - Print the number of cities you need to go through (K), the path, and the total time.
+            System.out.println(dist +1);        // - Print the distance.
+            for (Integer x : path) {            // - Print the path.
+                System.out.print(x + " ");
+            }
+            System.out.println("\n" + (time));  // - Print the time.       
             
-            for (int i = 1; i < (nmtc[0])+1; i++) {                   //nmtc[0] = n
-                graph.addVertex(i);                                   // Instantiates the airports from 1 to M.
-            }            
-            
-            getConnections(graph, (nmtc[1]));                       // Calls our second input method with value m. (nmtc[1] = m) to create the bidirectional roads.
-            int[] direction = getStartAndEndPoints(graph);          // Calls our third and last input method to get the starting and ending cities.
-            
-            
-            
-        } catch (NumberFormatException e) {
-            System.out.println("Something is wrong!");
+        } catch (InputMismatchException e) {    // - Exceptions
+            System.out.println("Something is wrong with the input!");
         }
     }
     
-    public static void getConnections(Graph graph, int m) {  //Gets the next input lines
-        Scanner scan = new Scanner(System.in);  // each line consist of 2 numbers
-        String[] road_str = new String[m];     // in which they indicate source and destination values..
-        
-        for (int i = 0; i < m; i++) {           // Catch the input values.
-            road_str[i] = scan.nextLine();      // Each line is kept as one variable.
-        }    
-        for (String s : road_str) {             // Iterate each line, use split to get the source and destination values seperately.
-            String[] sourceAndDestinaiton_str = new String[2];
-            sourceAndDestinaiton_str = s.split(" ");
-            
-            Integer src = Integer.valueOf(sourceAndDestinaiton_str[0]); // Assigns the src and des values.
-            Integer des = Integer.valueOf(sourceAndDestinaiton_str[1]); checkValidAirport(graph,src,des);//Checks if these values are valid.
-            
-            graph.addEdge(src, des);
-        }    
-    }
-    
-    public static int[] getStartAndEndPoints(Graph graph) {    // Returns int array FirstandLastCity containing the start and end points.
-        Scanner scan = new Scanner(System.in);
-        String lastLine = scan.nextLine();
-        String[] v = new String[2];
-        v = lastLine.split(" ");
-        
-        int[] FirstandLastCity = {Integer.valueOf(v[0]), Integer.valueOf(v[1])};    //FirstandLastCity[0] = First City (Starting point)
-                                                                                    //FirstandLastCity[1] = Last City (End point)
-        checkValidAirport(graph, FirstandLastCity[0], FirstandLastCity[1]);
-                                                                            // Checks if the starting and the ending cities are valid or not.
-        return (FirstandLastCity);
-    }
-    
-    
-    public static void checkValidAirport(Graph graph, int n, int m) {      // Checks if there is an airport or not with the given user inputs.
-        if (!graph.hasVertex(n)) {
-            System.out.println("There is no such airport with the name: " + n); System.exit(0);
-        }if (!graph.hasVertex(m)) {
-            System.out.println("There is no such airport with the name: " + m); System.exit(0);
-        }if (n == m) {
-            System.out.println("Source and destination cannot be the same: " + n + " = " + m); System.exit(0);
+    // - Checks if the input vertex is valid or not.
+    public static void checkValidVertex(Graph graph, int n, int m) {
+        if ((n > graph.V()) || (n <= 0)) {
+            System.out.println("There is no such airport with the code: " + n); 
+            System.exit(0);
+        }
+        if ((m > graph.V()) || (m <= 0)) {
+            System.out.println("There is no such airport with the code: " + n); 
+            System.exit(0);
+        }
+        if (n == m) {
+            System.out.println("Source and destination cannot be the same: (" + n + " = " + m + ")"); 
+            System.exit(0);
         }      
-    }
-    
-    public static int travel(int t){
-        
-        
-        return t;
-    }
-    
-    public static void checkAccessibility(Graph g, int src, int des){
-        if(g.hasEdge(src, des))
-            System.out.println("1");
     }
     
 }

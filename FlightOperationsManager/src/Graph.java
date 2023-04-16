f40ab12fe14c45ce04a8660c6de695fd0248a487
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2023-04-16 19:30:46 +0300

    Update Graph.java

--- FlightOperationsManager/src/Graph.java
+++ FlightOperationsManager/src/Graph.java
@@ -1,61 +1,67 @@
+//-----------------------------------------------------
+// Title: Graph class
+// Author: Mustafa Baran Ercan
+// ID: 28810555206
+// Section: 1
+// Homework: 1
+// Description: This class is the Undirectional Graph implementation class.
+//-----------------------------------------------------
 import java.util.*;
 
-public class Graph<T> { // Generic class of Graph, T stands for "Type"
-    
-// Use Hashmap to store the edges of graph.
-    private Map<T, List<T> > map = new HashMap<>();
-    
-    public void addVertex(T s) {    // This method adds a new vertex to the graph.
-        map.put(s, new LinkedList<T>());
-    }
-    
-    public void addEdge(T source, T destination) {  // This method adds ther edge and the vertices if they don't exist 
-                                                    // Standart add edge method works with a boolean parameter called bidirection.
-        if(!map.containsKey(source))                // Since all of our edges are undirected, we don't need to keep that unnecessary information.
-            addVertex(source);
-        if (!map.containsKey(destination))
-            addVertex(destination);
-        
-        map.get(source).add(destination);
-        map.get(destination).add(source);
-        
-    }
-    
-    public int getVertexCount() {       // This method returns the # of vertices
-        return (map.keySet().size());
-    }
-    
-    public int getEdgeCount() {         //This method returns the # of edges
-        int count = 0;                  
-        for (T v : map.keySet()) {
-            count += map.get(v).size();
-        }
-        return (count/2);               // Since all edges are bidirectional, we count them twice. So the real count is half of it.
-    }
+    public class Graph {
+        private final int V;                                // - Number of vertices in the graph.
+        private List<Integer>[] adjList;                    // - Adjacency list for each element.
+        private int E = 0;                                  // - Number of edges in the graph.
 
-    public boolean hasVertex(T s) {     // If the graph has that vertex; return true, else retuns false.
-        return (map.containsKey(s));
-    }
-    
-    public boolean hasEdge(T src, T des) { // If the graph has that edge; return true, else retuns false.
-        if (map.get(src).contains(des)) return true;
-        else return false;
-    }
-    
-    
-    public String toString() {
-        StringBuilder builder = new StringBuilder();
-        
-        for (T v : map.keySet()) {
-            builder.append(v.toString() + ": ");
-            for (T w: map.get(v)) {
-                builder.append(w.toString() + " ");
+        public Graph(int V) {                               // - Constructor to create a graph with V vertices..
+            this.V = V;                                         
+            adjList = new List[V+1];                        // - Initialize adjacency list for each vertex.
+            for (int i = 1; i < V + 1; i++) {               // - (V + 1) instead of (V) because airport indexes start from 1 not 0.
+                adjList[i] = new ArrayList<Integer>();
             }
-            builder.append("\n");
         }
-        return (builder.toString());
+
+    public void addEdge(int v, int w) {                     // - Method to add an undirected edge between vertices v and w.
+            adjList[v].add(w);                              // - Add w to v's adjacency list.
+            adjList[w].add(v);                              // - Vice versa.
+            E++;                                            // - Increase the E by 1.
+        }
+
+        public int V() {                                    // - Method to get the number of vertices in the graph
+            return V;
+        }    
+
+    public int BFS(int src, int des, List<Integer> shortestPath) {  // - This method uses Breadth First Search algorithm to calculate the shortest path and the distance of that path.
+        int[] dist = new int[V+1];                                  // - The method takes three inputs, src, des (represent the source and destination) and shortestPath.
+        int[] prev = new int[V+1];                                  // - Distance array stores the minimum distances to src from all other nodes.
+        Arrays.fill(dist, Integer.MAX_VALUE);                       // - Prev array stores the previous node.
+        Arrays.fill(prev, -1);                                      // - Set all the distances to maximum value and previous nodes to -1 initially.
+        dist[src] = 0;                                              // - (Distance from src to src is 0)
+        Queue<Integer> q = new LinkedList<Integer>();               // - A queue is used to perform the BFS traversal.
+        q.add(src);
+        while (!q.isEmpty()) {                                      // - While the queue is not empty, the algorithm dequeues node u,
+            int u = q.poll();                                       // - and checks the adjacency list for all of its neighbors.
+            for (int v : adjList[u]) {                              // - for each neighbor:
+                if (dist[u] + 1 < dist[v]) {                        // - if the distance to v from src through u is shorter than the current distance to v update the values.
+                    dist[v] = dist[u] + 1;
+                    prev[v] = u;                    
+                    q.add(v);
+                }
+            }
+        }
+        if (prev[des] == -1) {                              // - If there is no path from src to des return -1
+            return -1;
+        } else {
+            shortestPath.clear();                           // - Else, first clears the shortest path list.
+            int u = des;                                    // - Starts from destination node until it reaches to the source and adds each node.
+            while (u != src) {
+                shortestPath.add(u);
+                u = prev[u];
+            }
+            shortestPath.add(src);                          // - Finaly adds the source node.
+            Collections.reverse(shortestPath);              // - Reverses the path or else it will be from destination to source.
+            return dist[des];                               // - Returns the shortest distance from src to des.
+        }
     }
-
-
 
 }
